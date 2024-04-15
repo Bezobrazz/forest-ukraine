a2bfd5a27cfc1d698a05b1c557c96416d24f64f5
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-04-15 21:30:43 +0300

    fixed nav styles

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,39 +1,59 @@
 import { AiFillCloseSquare } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import s from "./Sidebar.module.css";
+import styles from "./Sidebar.module.css";
 
 const Sidebar = ({ toggleSidebar, closeSidebar }) => {
+  const navItems = [
+    { to: "/", text: "Головна" },
+    {
+      to: "/production",
+      text: "Виробництво",
+      subItems: [
+        { to: "/production/finished-products", text: "Виготовлена Продукція" },
+      ],
+    },
+  ];
+
+  const handleNavLinkClick = () => {
+    closeSidebar();
+  };
+
   return (
-    <div className={s.sidebar}>
-      <div className={s.topBar}>
-        <h2 className={s.sidebarTitle}>Navigation</h2>
+    <div className={styles.sidebar}>
+      <div className={styles.topBar}>
+        <h2 className={styles.sidebarTitle}>Navigation</h2>
       </div>
-      <ul className={s.navWrapper}>
-        <li>
-          <Link className={s.navLink} to="/" onClick={closeSidebar}>
-            Головна
-          </Link>
-        </li>
-        <li>
-          <Link className={s.navLink} to="/production" onClick={closeSidebar}>
-            Виробництво
-          </Link>
-          <ul>
-            <li>
-              <Link
-                className={s.navLink}
-                to="/production/finished-products"
-                onClick={closeSidebar}
-              >
-                Виготовлена Продукція
-              </Link>
-            </li>
-          </ul>
-        </li>
+      <ul className={styles.navWrapper}>
+        {navItems.map((item, index) => (
+          <li key={index}>
+            <Link
+              to={item.to}
+              className={styles.navMainLinks}
+              onClick={handleNavLinkClick}
+            >
+              {item.text}
+            </Link>
+            {item.subItems && (
+              <ul className={styles.navSubitemsWrapper}>
+                {item.subItems.map((subItem, subIndex) => (
+                  <li className={styles.navSubitem} key={subIndex}>
+                    <Link
+                      to={subItem.to}
+                      className={styles.navSubLink}
+                      onClick={handleNavLinkClick}
+                    >
+                      {subItem.text}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
       </ul>
       <AiFillCloseSquare
         onClick={toggleSidebar}
-        className={s.closeSidebarBtn}
+        className={styles.closeSidebarBtn}
       />
     </div>
   );
