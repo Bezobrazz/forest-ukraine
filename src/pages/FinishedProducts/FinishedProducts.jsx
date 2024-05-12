6f4ba1bdefce8c27ad213b80e0fd05f750689ad5
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-05-12 16:24:21 +0300

    added date picker

--- src/pages/FinishedProducts/FinishedProducts.jsx
+++ src/pages/FinishedProducts/FinishedProducts.jsx
@@ -1,4 +1,16 @@
+import DatePicker from "react-datepicker";
+import { registerLocale } from "react-datepicker";
+import { useState } from "react";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+import uk from "date-fns/locale/uk";
+
+registerLocale("uk", uk);
+
 const FinishedProducts = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  console.log(startDate);
   return (
     <div>
       <h2>Виготовлена Продукція</h2>
@@ -10,6 +22,12 @@ const FinishedProducts = () => {
             <p>Кількість</p>
           </li>
         </ul>
+        <DatePicker
+          locale="uk"
+          selected={startDate}
+          dateFormat="dd/MM/yyyy"
+          onChange={(date) => setStartDate(date)}
+        />
       </div>
     </div>
   );
