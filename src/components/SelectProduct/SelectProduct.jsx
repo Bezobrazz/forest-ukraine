1683207ea5acd53a55bb9ae633ff7dce07d911ed
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-05-13 15:25:33 +0300

    edit styles of inputs

--- src/components/SelectProduct/SelectProduct.jsx
+++ src/components/SelectProduct/SelectProduct.jsx
@@ -1,21 +1,53 @@
 import Select from "react-select";
 import { useState } from "react";
+import s from "./SelectProduct.module.css";
 
 const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
+  { value: "кора крупна", label: "Кора Крупна" },
+  { value: "кора середня", label: "Кора Середня" },
+  { value: "кора дрібна", label: "Кора Дрібна" },
+  { value: "кора відсів 2", label: "Кора Вдсів 2" },
+  { value: "кора відсів 1", label: "Кора Вдсів 1" },
+  { value: "субстрат", label: "Субстрат" },
 ];
-const SelectProduct = () => {
+const SelectProduct = ({ onChange }) => {
   const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionChange = (selected) => {
+    setSelectedOption(selected);
+    onChange(selected);
+  };
   return (
-    <>
+    <div className={s.selectWrapper}>
       <Select
         defaultValue={selectedOption}
-        onChange={setSelectedOption}
+        onChange={handleOptionChange}
         options={options}
+        placeholder="Виберіть продукцію"
+        styles={{
+          container: (base) => ({
+            ...base,
+            width: "290px",
+            height: "35px",
+          }),
+          placeholder: (base) => ({
+            ...base,
+            fontSize: "14px",
+          }),
+          menu: (base) => ({
+            ...base,
+            color: "gray",
+            fontSize: "14px",
+          }),
+          control: (base) => ({
+            ...base,
+            border: "none",
+            minHeight: "35px",
+            borderRadius: "3px",
+          }),
+        }}
       />
-    </>
+    </div>
   );
 };
 
--- src/pages/FinishedProducts/FinishedProducts.jsx
+++ src/pages/FinishedProducts/FinishedProducts.jsx
@@ -1,18 +1,42 @@
+import { forwardRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import { registerLocale } from "react-datepicker";
-import { useState } from "react";
-import s from "./FinishedProducts.module.css";
-
-import "react-datepicker/dist/react-datepicker.css";
-
 import uk from "date-fns/locale/uk";
+import "react-datepicker/dist/react-datepicker.css";
 import SelectProduct from "../../components/SelectProduct/SelectProduct.jsx";
+import s from "./FinishedProducts.module.css";
 
 registerLocale("uk", uk);
 
+const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+  <button className={s.datePickerCustomInput} onClick={onClick} ref={ref}>
+    {value}
+  </button>
+));
+ExampleCustomInput.displayName = "ExampleCustomInput";
+
 const FinishedProducts = () => {
   const [startDate, setStartDate] = useState(new Date());
-  console.log(startDate);
+  const [selectedProduct, setSelectedProduct] = useState("");
+  const { label } = selectedProduct;
+  const [quantity, setQuantity] = useState("");
+
+  const handleProductChange = (selected) => {
+    setSelectedProduct(selected);
+  };
+
+  const handleQuantityChange = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Тут ти можеш обробити дані форми
+    console.log("Дата:", startDate);
+    console.log("Вибраний продукт:", label);
+    console.log("Кількість:", typeof quantity);
+  };
+
   return (
     <div>
       <h2>Виготовлена Продукція</h2>
@@ -24,15 +48,26 @@ const FinishedProducts = () => {
             <p>Кількість</p>
           </li>
         </ul>
-        <div className={s.inputsFieldsWrapper}>
+        <form onSubmit={handleSubmit} className={s.inputsFieldsWrapper}>
           <DatePicker
             locale="uk"
             selected={startDate}
             dateFormat="dd/MM/yyyy"
             onChange={(date) => setStartDate(date)}
+            customInput={<ExampleCustomInput />}
           />
-          <SelectProduct />
-        </div>
+          <SelectProduct onChange={handleProductChange} />
+          <input
+            className={s.quantityInput}
+            type="number"
+            value={quantity}
+            onChange={handleQuantityChange}
+            placeholder="Кількість"
+          />
+          <button className={s.saveButton} type="submit">
+            Зберегти
+          </button>
+        </form>
       </div>
     </div>
   );
