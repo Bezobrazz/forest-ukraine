2ab86d1c60dc229a6482c66698a9fed66d42fb64
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-05-14 15:51:35 +0300

    added env

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -1,17 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCfzRpbS5AOusNdlNVgvaVx0EJ2_dNnHE4",
-  authDomain: "forest-ukraine.firebaseapp.com",
-  projectId: "forest-ukraine",
-  storageBucket: "forest-ukraine.appspot.com",
-  messagingSenderId: "349220888327",
-  appId: "1:349220888327:web:80e5c4f771b24ea6b16fa2",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
