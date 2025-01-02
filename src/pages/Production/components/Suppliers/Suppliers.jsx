15ed0d0c20e4eec367711fe0fda0df3a94d8f9d3
Bezobrazz <kostenko.uvd@gmail.com>
2025-01-02 15:08:06 +0200

    added top bar

--- src/pages/Production/components/Suppliers/Suppliers.jsx
+++ src/pages/Production/components/Suppliers/Suppliers.jsx
@@ -1,3 +1,4 @@
+import { BiSort } from "react-icons/bi";
 import Button from "../../../../components/Button/Button.jsx";
 import SearchInput from "../../../../components/SearchInput/SearchInput.jsx";
 import styles from "./Suppliers.module.css";
@@ -5,14 +6,17 @@ import styles from "./Suppliers.module.css";
 export const Suppliers = () => {
   return (
     <div>
-      <div>
+      <div className={styles.topBar}>
         <div className={styles.searchWrapper}>
           <SearchInput
             placeholder="Пошук постачальників..."
             onSearch={() => {}}
           />
-          <Button variant="secondary">Додати постачальника</Button>
+          <Button variant="secondary">
+            <BiSort />
+          </Button>
         </div>
+        <Button variant="primary">Додати постачальника</Button>
       </div>
     </div>
   );
