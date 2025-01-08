9a72e542d79c4ee1376e7c627e9bb452db6a01b1
Bezobrazz <kostenko.uvd@gmail.com>
2025-01-08 16:09:38 +0200

    upd paths

--- src/Firebase/AddFinishedProducts.jsx
+++ src/Firebase/AddFinishedProducts.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-import { addProduct } from "./firebaseServices.js";
-
 const AddFinishedProducts = () => {
-//   const addFinishedProducts = async () => {
-//     const docRef = await addProduct() {
-//       date: startDate,
-//       product: selectedProduct,
-//       quantity: quantity,
-//     };
-//   };
-//   return <div></div>;
-// };
-
+  //   const addFinishedProducts = async () => {
+  //     const docRef = await addProduct() {
+  //       date: startDate,
+  //       product: selectedProduct,
+  //       quantity: quantity,
+  //     };
+  //   };
+  //   return <div></div>;
+  // };
+};
 export default AddFinishedProducts;
