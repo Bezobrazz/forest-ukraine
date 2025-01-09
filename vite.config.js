e1cba35b2b52e9bb02f1d6456d1a7d03beea2c9f
Bezobrazz <kostenko.uvd@gmail.com>
2025-01-09 13:00:50 +0200

    upd vite config for ignoring "../FirebaseServices.js" path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["../FirebaseServices.js"], // Вказуємо файли або модулі, які треба ігнорувати
+    },
+  },
   server: {
     proxy: {
       "/keepincrm": {
