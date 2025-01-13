f7bbc492db06840b56603417b5bb765dfeba8e4a
Bezobrazz <kostenko.uvd@gmail.com>
2025-01-13 10:36:42 +0200

    add responsive button and top bar styles to Bags component

--- src/pages/Production/components/Bags/Bags.jsx
+++ src/pages/Production/components/Bags/Bags.jsx
@@ -1,3 +1,20 @@
+import styles from "./Bags.module.css";
+import Button from "../../../../components/Button/Button.jsx";
+import { useMediaQuery } from "react-responsive";
+
 export const Bags = () => {
-  return <div>Bags</div>;
+  const isMobile = useMediaQuery({ query: "(max-width: 425px)" });
+  return (
+    <div>
+      <div className={styles.topBar}>
+        <Button
+          variant="primary"
+          width={isMobile ? "100%" : "230px"}
+          // onClick={handleOpenAddModal}
+        >
+          Додати постачальника
+        </Button>
+      </div>
+    </div>
+  );
 };
